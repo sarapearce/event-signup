684936db0e0c2805e977f3a462f054457f25532f
Sara <sara@leftturnonly.tv>
2018-01-15 18:39:23 +0000

    initial commit, dependencies, view files made

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fsEventSignupApp
+ * @description
+ * # fsEventSignupApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fsEventSignupApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fsEventSignupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fsEventSignupApp
+ */
+angular.module('fsEventSignupApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

326fcc26eb7fdd9a03ca44d15faaaa240d3a72d7
Sara <sara@leftturnonly.tv>
2018-01-18 16:07:52 +0000

    added README.md template, wrote design prototype, made some controller files

---
+++ app/scripts/controllers/welcome.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fsEventSignupApp.controller:WelcomeCtrl
+ * @description
+ * # WelcomeCtrl
+ * Controller of the fsEventSignupApp
+ */
+angular.module('fsEventSignupApp')
+  .controller('WelcomeCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
